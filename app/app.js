19c6f7f6f421b1da680c6089edbdf72705d1486e
Will Smith <WSmith@scottlogic.com>
2015-12-21 11:04:16 +0000

    Created some fake json data

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
+angular.module('myApp', ['ngRoute']).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  })
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
