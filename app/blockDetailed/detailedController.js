503595a5adb350cd486b72ff09cf2e9d8e1647dd
Will Smith <WSmith@scottlogic.com>
2015-12-22 10:58:35 +0000

    Created a dummy detailed view

--- app/blockDetailed/detailedController.js
+++ app/blockDetailed/detailedController.js
@@ -1,7 +1,10 @@
 
 
 angular.module("detailedBlock", ["ngRoute"])
-.controller('DetailedBlockController',  function() {
+.controller('DetailedBlockController',  function($http) {
     var controller=this;
-
+    var currentBlock;
+    $http.get('resources/00000000000000000a5093e24e1f43c509e2cd51dd3ec38c739a532523534.json').success(function(data) {
+      controller.currentBlock = data;
+    });
 });
