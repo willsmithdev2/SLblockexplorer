1eff38853a57fad8a42e66b5732d6af0ae2a4825
Will Smith <WSmith@scottlogic.com>
2015-12-22 16:00:56 +0000

    Formatted time outside of view so it can be filtered on

--- app/blocktable/controller.js
+++ app/blocktable/controller.js
@@ -1,25 +1,24 @@
 
 
 angular.module("blockTable", ["ngRoute"])
-.controller('BlockTableController',  function($http) {
+.controller('BlockTableController',  function($filter, $http) {
 
     var controller=this;
     controller.latestBlockHash;
     controller.blockChain=[];
 
-
     $http.get('https://blockexplorer.com/api/status?q=getLastBlockHash').success(function(data) {
       controller.latestBlockHash = data.lastblockhash;
-      controller.somestring=data.lastblockhash;
     }).then(function(result){
       controller.generateBlockChain(controller.latestBlockHash);
     });
 
     this.generateBlockChain = function(hash){
       $http.get('https://blockexplorer.com/api/block/'+hash).success(function(data) {
+        var formattedDate=$filter('date')(data.time*1000,'dd/MM/yyyy h:mma');
         controller.blockChain.push({
           "hash":data.hash,
-          "time":data.time,
+          "time": formattedDate,
           "height":data.height
         })
         if(!data.previousblockhash){
