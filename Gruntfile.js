cdcadabb539809992b586fbdae4045a712ef4279
Will Smith <WSmith@scottlogic.com>
2016-01-07 09:42:09 +0000

    Can run tests from Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,44 @@ module.exports = function(grunt) {
         src: 'app/**/*.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    concat: {
+      options: {},
+      dist: {
+        files: {
+          'public/js/app.js': [
+             'src/js/**/*.js',
+             '!unit-tests/**/*.js'
+           ]
+        }
+      }
+    },
+
+    karma: {
+      unit: {
+        options: {
+          frameworks: ['jasmine'],
+          singleRun: true,
+          browsers: ['PhantomJS'],
+          files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/components/**/*.js',
+            'app/controllers/*.js',
+            'unit-tests/**/*_test.js'
+          ]
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
+  grunt.loadNpmTasks('grunt-karma');
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','karma']);
 
 };
