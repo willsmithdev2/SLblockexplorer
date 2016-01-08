02c3fefad16415764d1ba51e3c920a5c0cf72c4b
Will Smith <WSmith@scottlogic.com>
2016-01-08 13:50:50 +0000

    Changed score data to be transactions

--- app/block-detail/controller.js
+++ app/block-detail/controller.js
@@ -3,11 +3,11 @@ define([], function() {
   function BlockDetailController($scope,$http,$routeParams) {
     var controller=this;
     var currentBlock;
-    $scope.scoreData;
+    $scope.transactions;
 
     $http.get('https://blockexplorer.com/api/block/'+$routeParams.blockid).success(function(data) {
       controller.currentBlock = data;
-      $scope.scoreData=data.tx;
+      $scope.transactions=data.tx;
     });
   }
 
