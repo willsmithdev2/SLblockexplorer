acdd1c7ff03bceb769ec7753640ce47cfa426af2
Will Smith <WSmith@scottlogic.com>
2016-01-08 13:52:37 +0000

    Renamed d3bars to transaction graph

--- app/block-detail/module.js
+++ app/block-detail/module.js
@@ -4,7 +4,7 @@ define(['angular', './controller', './directive', '../d3/module','./d3Directive'
     'use strict';
 
     var app = angular.module('BlockDetailModule', ['ngRoute',"d3"]);
-    app.directive('d3Bars',D3Directive);
+    app.directive('transactionGraph',D3Directive);
     app.controller('BlockDetailCtrl', BlockDetailController);
     app.directive('blockDetail', BlockDetailDirective);
 
