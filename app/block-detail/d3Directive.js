54f362f2fc9db506e057f01095a48c9ff5565179
Will Smith <WSmith@scottlogic.com>
2016-01-08 14:03:32 +0000

    Code cleanup

--- app/block-detail/d3Directive.js
+++ app/block-detail/d3Directive.js
@@ -1,6 +1,5 @@
 define([], function() {
 
-
   function D3Directive($window, $timeout, $http,d3Service) {
     return {
       restrict :'A',
@@ -9,125 +8,126 @@ define([], function() {
       },
       link: function(scope, ele, attrs) {
 
-            var width = 640,
-            height = 480;
+        var width = 640,
+        height = 480;
 
-            var nodes = [];
+        var nodes = [];
 
-            var links = [];
+        var links = [];
 
-            var generateLinksAndNodes = function(data){
-                var numOfTxs=data.length;
-                for(var i = 0; i < numOfTxs; i++){
-                  var obj={source:i,target:i+1};
-                  var node={x:(width/2) ,y:i*100, hash:data[i]};
-                  nodes.push(node);
-                  if(i<numOfTxs-1){
-                    links.push(obj);
-                  }
-                }
+        var generateLinksAndNodes = function(data){
+          var numOfTxs=data.length;
+          for(var i = 0; i < numOfTxs; i++){
+            var obj={source:i,target:i+1};
+            var node={x:(width/2) ,y:i*100, hash:data[i]};
+            nodes.push(node);
+            if(i<numOfTxs-1){
+              links.push(obj);
             }
+          }
+        }
 
-            d3Service.d3().then(function(d3) {
+        d3Service.d3().then(function(d3) {
 
-           //Watch 'data' and run scope.render whenever it changes
-            scope.$watch('data', function(){
-                scope.render(scope.data);
-            }, true);
+          //Watch 'data' and run scope.render whenever it changes
+          scope.$watch('data', function(){
+            scope.render(scope.data);
+          }, true);
 
-            scope.render = function(data) {
-              if(!(!data)){
+          scope.render = function(data) {
+            if(!(!data)){
 
-                generateLinksAndNodes(data);
+              generateLinksAndNodes(data);
 
-                var svg = d3.select(ele[0]).append('svg')
-                .attr("width", width)
-                .attr("height", height)
-                .call(d3.behavior.zoom().scaleExtent([0.5, 8]).on("zoom", zoom))
-                .append("g");
+              var svg = d3.select(ele[0]).append('svg')
+              .attr("width", width)
+              .attr("height", height)
+              .call(d3.behavior.zoom().scaleExtent([0.5, 8]).on("zoom", zoom))
+              .append("g");
 
-                var force = d3.layout.force()
-                    .size([width, height])
-                    .nodes(nodes)
-                    .links(links);
+              var force = d3.layout.force()
+              .size([width, height])
+              .nodes(nodes)
+              .links(links);
 
-                force.gravity(0);
-                force.linkDistance(100);
+              force.gravity(0);
+              force.linkDistance(100);
 
 
-                var link = svg.selectAll('.link')
-                    .data(links)
-                    .enter().append('line')
-                    .attr('class', 'link');
+              var link = svg.selectAll('.link')
+              .data(links)
+              .enter().append('line')
+              .attr('class', 'link');
 
-                var node = svg.selectAll('g.node')
-                    .data(nodes)
-                    .enter()
-                    .append("g");
+              var node = svg.selectAll('g.node')
+              .data(nodes)
+              .enter()
+              .append("g");
 
-                var texts = node.append("text")
-                  .attr("class", "label")
-                  .attr("fill", "black")
-                  .attr("text-anchor", "middle");
+              var texts = node.append("text")
+              .attr("class", "label")
+              .attr("fill", "black")
+              .attr("text-anchor", "middle");
 
-                force.on('end', function() {
-                    node.append("circle")
-                    .attr('class', 'node')
-                    .on("click", click);
+              force.on('end', function() {
+                node.append("circle")
+                .attr('class', 'node')
+                .on("click", click);
 
-                    node.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; });
-                    node.selectAll(".node")
-                    .attr('r', width/100);
+                node.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")"; });
+                node.selectAll(".node")
+                .attr('r', width/100);
 
-                    link.attr('x1', function(d) { return d.source.x; })
-                        .attr('y1', function(d) { return d.source.y; })
-                        .attr('x2', function(d) { return d.target.x; })
-                        .attr('y2', function(d) { return d.target.y; });
+                link.attr('x1', function(d) { return d.source.x; })
+                .attr('y1', function(d) { return d.source.y; })
+                .attr('x2', function(d) { return d.target.x; })
+                .attr('y2', function(d) { return d.target.y; });
 
-                    texts.text(function(d) {  return d.hash;  })
-                    .attr("transform", function(d) {
-                        return "translate(" + (220) + "," + (0) + ")";
-                    });
+                texts.text(function(d) {  return d.hash;  })
+                .attr("transform", function(d) {
+                  return "translate(" + (220) + "," + (0) + ")";
                 });
+              });
 
-                force.start();
-
-
-                function zoom() {
-                    svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-                }
+              force.start();
 
 
-                function click(data) {
-                    //reset other elements
-                    d3.selectAll('.node')
-                    .style('fill', null)
-                    .attr("r", width/100);
+              function zoom() {
+                svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+              }
+              
+              function click(data) {
+                //reset other elements
+                d3.selectAll('.node')
+                .style('fill', null)
+                .attr("r", width/100);
 
-                    node.selectAll('.coinValue')
-                    .remove();
+                node.selectAll('.coinValue')
+                .remove();
 
-                    //fill in blue
-                    d3.select(this).transition()
-                    .duration(750)
-                    .attr("r", 30)
-                    .style("fill", "lightsteelblue");
+                //fill in blue
+                d3.select(this).transition()
+                .duration(750)
+                .attr("r", 30)
+                .style("fill", "lightsteelblue");
 
-                    $http.get('https://blockexplorer.com/api/tx/'+data.hash).success(function(txResult) {
-                      node.filter(function (o) {return o.index === data.index;})
-                      .append("text")
-                      .attr("class", "coinValue")
-                      .attr("fill", "black")
-                      .attr("text-anchor", "middle")
-                      .text(Math.round(txResult.valueOut*10)/100);
-                    });
-                }
+                $http.get('https://blockexplorer.com/api/tx/'+data.hash).success(function(txResult) {
+                  node.filter(function (o) {return o.index === data.index;})
+                  .append("text")
+                  .attr("class", "coinValue")
+                  .attr("fill", "black")
+                  .attr("text-anchor", "middle")
+                  .text(Math.round(txResult.valueOut*10)/100);
+                });
               }
             }
-      });
+          }
+        });
+      }
     }
   }
-}
+
+
 
   D3Directive.$inject=['$window', '$timeout','$http', 'd3Service'];
   return D3Directive;
--- app/block-detail/module.js
+++ app/block-detail/module.js
@@ -1,11 +1,11 @@
 
 define(['angular', './controller', './directive', '../d3/module','./d3Directive'],
-    function(angular, BlockDetailController, BlockDetailDirective, d3,D3Directive){
-    'use strict';
+function(angular, BlockDetailController, BlockDetailDirective, d3,D3Directive){
+  'use strict';
 
-    var app = angular.module('BlockDetailModule', ['ngRoute',"d3"]);
-    app.directive('transactionGraph',D3Directive);
-    app.controller('BlockDetailCtrl', BlockDetailController);
-    app.directive('blockDetail', BlockDetailDirective);
+  var app = angular.module('BlockDetailModule', ['ngRoute',"d3"]);
+  app.directive('transactionGraph',D3Directive);
+  app.controller('BlockDetailCtrl', BlockDetailController);
+  app.directive('blockDetail', BlockDetailDirective);
 
 });
