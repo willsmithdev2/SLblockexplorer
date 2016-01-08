41433569da24a03b756d87959463bb0a4b841e80
Will Smith <WSmith@scottlogic.com>
2016-01-08 13:49:12 +0000

    Changed d3Directive to attribute to fix border issue

--- app/block-detail/module.js
+++ app/block-detail/module.js
@@ -1,10 +1,11 @@
 
-define(['angular', './controller', './directive'],
-    function(angular, BlockDetailController, BlockDetailDirective){
+define(['angular', './controller', './directive', '../d3/module','./d3Directive'],
+    function(angular, BlockDetailController, BlockDetailDirective, d3,D3Directive){
     'use strict';
 
-    var app = angular.module('BlockDetailModule', ['ngRoute']);
-
+    var app = angular.module('BlockDetailModule', ['ngRoute',"d3"]);
+    app.directive('d3Bars',D3Directive);
     app.controller('BlockDetailCtrl', BlockDetailController);
     app.directive('blockDetail', BlockDetailDirective);
+
 });
