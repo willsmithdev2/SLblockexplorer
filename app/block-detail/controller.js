b524bd883e046f20c3f5ada603b762b3498df20a
Will Smith <WSmith@scottlogic.com>
2016-01-11 11:08:00 +0000

    Can check the hash of current block by hitting the endpoint on SLBitCoin project

--- app/block-detail/controller.js
+++ app/block-detail/controller.js
@@ -4,11 +4,26 @@ define([], function() {
     var controller=this;
     var currentBlock;
     $scope.transactions;
+    var isBlockValid;
+    controller.showButton=true;
+    controller.hashMessage=""
 
     $http.get('https://blockexplorer.com/api/block/'+$routeParams.blockid).success(function(data) {
       controller.currentBlock = data;
       $scope.transactions=data.tx;
     });
+
+    controller.checkHash=function(){
+      $http.post('http://localhost:3000/api/validate-hash',controller.currentBlock).success(function(data) {
+        controller.isBlockValid=data[0].isHashValid;
+        if(controller.isBlockValid){
+          controller.hashMessage="This blocks hash is valid";
+        }else{
+          controller.hashMessage="This blocks hash is not valid!";
+        }
+        controller.showButton=false;
+      });
+    }
   }
 
   BlockDetailController.$inject=['$scope','$http','$routeParams'];
